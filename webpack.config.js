0f3e23d1079d5e5e267a0480702f26ef95169ed8
Ashes <ashesdangol@hotmail.com>
2021-04-05 23:51:14 +1000

    completed build process configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,54 @@
+const currentTask = process.env.npm_lifecycle_event
 const { watch } = require("fs")
 const path = require("path")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const fse = require("fs-extra")
+
 const postCssPlugins = [require("postcss-import"), require("postcss-mixins"), require("postcss-simple-vars"), require("postcss-nested"), require("postcss-hexrgba"), require("autoprefixer")]
 
-module.exports = {
-  // path to the file what we want to bundle App.js
+class RunAfterCompile {
+  apply(compiler) {
+    compiler.hooks.done.tap("Copy images", function () {
+      fse.copySync("./app/assets/images", "./docs/assets/images")
+    })
+  }
+}
+
+let cssConfig = {
+  test: /\.css$/i,
+  use: ["css-loader?url=false", { loader: "postcss-loader", options: { postcssOptions: { plugins: postCssPlugins } } }]
+}
+
+let pages = fse
+  .readdirSync("./app")
+  .filter(function (file) {
+    return file.endsWith(".html")
+  })
+  .map(function (page) {
+    return new HtmlWebpackPlugin({
+      filename: page,
+      template: `./app/${page}`
+    })
+  })
+
+let config = {
   entry: "./app/assets/scripts/App.js",
-  output: {
+  plugins: pages,
+  module: {
+    rules: [cssConfig]
+  }
+}
+
+if (currentTask == "dev") {
+  cssConfig.use.unshift("style-loader")
+  config.output = {
     filename: "bundled.js",
     path: path.resolve(__dirname, "app")
-  },
-  // watch Html inside any app folder or subfolder and  full relod
-  devServer: {
+  }
+  config.devServer = {
     before: function (app, server) {
       server._watch("./app/**/*.html")
     },
@@ -21,14 +59,36 @@ module.exports = {
     host: "0.0.0.0"
     // host allow all devices
     // find local ip of ur PC using ipconfig, IPv4 Address, 192.168.X.X:3000
-  },
-  mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader?url=false", { loader: "postcss-loader", options: { postcssOptions: { plugins: postCssPlugins } } }]
-      }
-    ]
   }
+  config.mode = "development"
 }
+
+if (currentTask == "build") {
+  config.module.rules.push({
+    test: /\.js$/,
+    exclude: /(node_modules)/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ["@babel/preset-env"]
+      }
+    }
+  })
+  cssConfig.use.unshift(MiniCssExtractPlugin.loader)
+  config.output = {
+    filename: "[name].[chunkhas].js",
+    chunkFilename: "[name].[chunkhas].js",
+    path: path.resolve(__dirname, "docs")
+  }
+  config.mode = "production"
+  config.optimization = {
+    splitChunks: {
+      chunks: "all"
+    },
+    minimize: true,
+    minimizer: [`...`, new CssMinimizerPlugin()]
+  }
+  config.plugins.push(new CleanWebpackPlugin(), new MiniCssExtractPlugin({ filename: "styles.[chunkhash].css" }), new RunAfterCompile())
+}
+
+module.exports = config
